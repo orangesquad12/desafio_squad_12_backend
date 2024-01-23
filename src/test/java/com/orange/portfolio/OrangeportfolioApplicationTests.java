85ef1dd7b7fc727081b1582d551c62ac2e90c7f1
Debora <silvadeborafisio@gmail.com>
2024-01-23 09:21:51 -0300

    Upload project

---
+++ src/test/java/com/orange/portfolio/OrangeportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeportfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
