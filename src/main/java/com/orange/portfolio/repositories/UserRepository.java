65548b1fb83b0f20ac1278a23c6e62582870ffaa
Debora <silvadeborafisio@gmail.com>
2024-01-24 11:14:49 -0300

    feat: inserting method implemented delete in the UserRepository interface

--- src/main/java/com/orange/portfolio/repositories/UserRepository.java
+++ src/main/java/com/orange/portfolio/repositories/UserRepository.java
@@ -4,6 +4,9 @@ import com.orange.portfolio.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+    void delete(Optional<User> user);
 }
