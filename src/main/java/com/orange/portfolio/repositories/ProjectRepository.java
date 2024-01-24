75098a823d5d1821d8858edca13fb175965ea1df
Debora <silvadeborafisio@gmail.com>
2024-01-24 10:46:43 -0300

    feat: inserting @Repository annotations into repository package interfaces

--- src/main/java/com/orange/portfolio/repositories/ProjectRepository.java
+++ src/main/java/com/orange/portfolio/repositories/ProjectRepository.java
@@ -3,6 +3,9 @@ package com.orange.portfolio.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.orange.portfolio.entities.Project;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface ProjectRepository extends JpaRepository <Project, Long> {
 
 }
