d13d7ac68c3fca31c9cd93b26a9ef98b23442218
Debora <silvadeborafisio@gmail.com>
2024-01-24 11:17:01 -0300

    feat: methods created in UserService and implemented in UserServiceImpl

--- src/main/java/com/orange/portfolio/services/UserService.java
+++ src/main/java/com/orange/portfolio/services/UserService.java
@@ -1,4 +1,10 @@
 package com.orange.portfolio.services;
 
+import com.orange.portfolio.entities.User;
+
 public interface UserService {
+
+    User save(User user);
+
+    void delete(Long id);
 }
--- src/main/java/com/orange/portfolio/services/impl/UserServiceImpl.java
+++ src/main/java/com/orange/portfolio/services/impl/UserServiceImpl.java
@@ -1,7 +1,25 @@
 package com.orange.portfolio.services.impl;
 
+import com.orange.portfolio.entities.User;
+import com.orange.portfolio.repositories.UserRepository;
+import com.orange.portfolio.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public User save(User user) {
+        return repository.save(user);
+    }
+
+    @Override
+    public void delete(Long id) {
+        var user = repository.findById(id);
+        repository.delete(user);
+    }
 }
