f02575f0f664d5378d63a3dc42b38bda040fbd18
Debora <silvadeborafisio@gmail.com>
2024-01-24 11:17:28 -0300

    feat: methods created in ProjectService and implemented in ProjectServiceImpl

--- src/main/java/com/orange/portfolio/services/ProjectService.java
+++ src/main/java/com/orange/portfolio/services/ProjectService.java
@@ -1,4 +1,12 @@
 package com.orange.portfolio.services;
 
+import com.orange.portfolio.entities.Project;
+
 public interface ProjectService {
+
+    Project save(Project project);
+
+    Project findProjectByTag(String tag);
+
+    void delete(Long id);
 }
--- src/main/java/com/orange/portfolio/services/impl/ProjectServiceImpl.java
+++ src/main/java/com/orange/portfolio/services/impl/ProjectServiceImpl.java
@@ -1,7 +1,30 @@
 package com.orange.portfolio.services.impl;
 
+import com.orange.portfolio.entities.Project;
+import com.orange.portfolio.repositories.ProjectRepository;
+import com.orange.portfolio.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ProjectServiceImpl {
+public class ProjectServiceImpl implements ProjectService {
+
+    @Autowired
+    private ProjectRepository repository;
+
+    @Override
+    public Project save(Project project) {
+        return repository.save(project);
+    }
+
+    @Override
+    public Project findProjectByTag(String tag) {
+        return repository.findProjectByTag(tag);
+    }
+
+    @Override
+    public void delete(Long id) {
+        var project = repository.findById(id);
+        repository.delete(project);
+    }
 }
