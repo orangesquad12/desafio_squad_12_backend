170e8183fa1062a9b446822ec8d0ef979c553a39
Debora <silvadeborafisio@gmail.com>
2024-01-24 11:14:01 -0300

    feat: inserting methods implemented findProjectById and delete in the ProjectRepository interface

--- src/main/java/com/orange/portfolio/repositories/ProjectRepository.java
+++ src/main/java/com/orange/portfolio/repositories/ProjectRepository.java
@@ -5,7 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.orange.portfolio.entities.Project;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProjectRepository extends JpaRepository <Project, Long> {
 
+    Project findProjectByTag(String tag);
+
+    void delete(Optional<Project> project);
 }
