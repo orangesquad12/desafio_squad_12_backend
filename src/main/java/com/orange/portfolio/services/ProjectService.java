f66ff62e1f8294832d58bf5de3e696776f70b069
Debora <silvadeborafisio@gmail.com>
2024-01-24 10:49:29 -0300

    feat: create ProjectService and ProjectServiceImpl

---
+++ src/main/java/com/orange/portfolio/services/ProjectService.java
@@ -0,0 +1,4 @@
+package com.orange.portfolio.services;
+
+public interface ProjectService {
+}
---
+++ src/main/java/com/orange/portfolio/services/impl/ProjectServiceImpl.java
@@ -0,0 +1,7 @@
+package com.orange.portfolio.services.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectServiceImpl {
+}
