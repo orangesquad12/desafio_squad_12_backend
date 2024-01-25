4aaef11b4af23181ee8a5c11b5a80fe445901bb5
Debora <silvadeborafisio@gmail.com>
2024-01-25 09:29:49 -0300

    feat: inserting new methods in ProjectRepository

--- src/main/java/com/orange/portfolio/repositories/ProjectRepository.java
+++ src/main/java/com/orange/portfolio/repositories/ProjectRepository.java
@@ -1,16 +1,20 @@
 package com.orange.portfolio.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.orange.portfolio.entities.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface ProjectRepository extends JpaRepository <Project, Long> {
 
     Project findProjectByTag(String tag);
 
-    void delete(Optional<Project> project);
+    void delete(Project project);
+
+    @Query(value = "select user_id from project p where p.user_id =?1",
+            nativeQuery = true)
+    List<Project> findAllByUserId(Long userId);
 }
