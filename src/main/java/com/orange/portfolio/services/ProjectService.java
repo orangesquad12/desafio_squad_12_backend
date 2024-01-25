f0b1faa5ac648175ebee057211889b484503600a
Debora <silvadeborafisio@gmail.com>
2024-01-25 09:28:30 -0300

    feat: inserting new methods in ProjectService and implementing them in ProjectServiceImpl

--- src/main/java/com/orange/portfolio/services/ProjectService.java
+++ src/main/java/com/orange/portfolio/services/ProjectService.java
@@ -1,12 +1,23 @@
 package com.orange.portfolio.services;
 
+import com.orange.portfolio.dtos.project.ProjectDTO;
 import com.orange.portfolio.entities.Project;
+
+import java.util.List;
 
 public interface ProjectService {
 
     Project save(Project project);
 
-    Project findProjectByTag(String tag);
+    Project create(ProjectDTO projectDTO);
+
+    Project getProjectByTag(String tag);
+
+    Project findProjectById(Long id);
+
+    List<Project> getAllByUserId(Long userId);
+
+    List<Project> getAllProjects();
 
     void delete(Long id);
 }
--- src/main/java/com/orange/portfolio/services/impl/ProjectServiceImpl.java
+++ src/main/java/com/orange/portfolio/services/impl/ProjectServiceImpl.java
@@ -1,10 +1,13 @@
 package com.orange.portfolio.services.impl;
 
+import com.orange.portfolio.dtos.project.ProjectDTO;
 import com.orange.portfolio.entities.Project;
 import com.orange.portfolio.repositories.ProjectRepository;
 import com.orange.portfolio.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProjectServiceImpl implements ProjectService {
@@ -18,8 +21,32 @@ public class ProjectServiceImpl implements ProjectService {
     }
 
     @Override
-    public Project findProjectByTag(String tag) {
+    public Project create(ProjectDTO projectDTO) {
+        Project project = new Project(projectDTO);
+        return this.save(project);
+    }
+
+    @Override
+    public Project getProjectByTag(String tag) {
         return repository.findProjectByTag(tag);
+    }
+
+    @Override
+    public Project findProjectById(Long id) {
+        return repository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public List<Project> getAllByUserId(Long userId) {
+        if (userId > 0){
+            return repository.findAllByUserId(userId);
+        } else return this.getAllProjects();
+    }
+
+
+    @Override
+    public List<Project> getAllProjects() {
+        return repository.findAll();
     }
 
     @Override
