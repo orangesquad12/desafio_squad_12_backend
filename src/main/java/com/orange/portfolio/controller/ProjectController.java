db4d55bf343477216e03f2c76d78427001d1af7e
Debora <silvadeborafisio@gmail.com>
2024-01-25 09:32:20 -0300

    feat: create ProjectController

---
+++ src/main/java/com/orange/portfolio/controller/ProjectController.java
@@ -0,0 +1,49 @@
+package com.orange.portfolio.controller;
+
+import com.orange.portfolio.dtos.project.ProjectDTO;
+import com.orange.portfolio.entities.Project;
+import com.orange.portfolio.services.impl.ProjectServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/project")
+public class ProjectController {
+
+    @Autowired
+    private ProjectServiceImpl projectService;
+
+    @PostMapping
+    public ResponseEntity<Project> save(@RequestBody ProjectDTO projectDTO){
+        var project = projectService.create(projectDTO);
+        return new ResponseEntity<>(project, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Project>> getAllByUserId(@RequestParam(value = "userId") Long userId){
+        var projectList = projectService.getAllByUserId(userId);
+        return new ResponseEntity<>(projectList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{tag}")
+    public ResponseEntity<Project> getProjectByTag(@PathVariable String tag){
+        var project = projectService.getProjectByTag(tag);
+        return new ResponseEntity<>(project, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Project> update(@PathVariable Long id, @RequestBody ProjectDTO projectDTO){
+        var findProject = projectService.findProjectById(id);
+        var saveProject = projectService.save(findProject);
+        return new ResponseEntity<>(saveProject, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        projectService.delete(id);
+    }
+}
