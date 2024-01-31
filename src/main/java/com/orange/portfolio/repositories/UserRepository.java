ffa9738d35a0d6a8762463cafc6a139e9f344d6a
Debora <silvadeborafisio@gmail.com>
2024-01-30 21:02:36 -0300

    feat: creating new method findByEmail

--- src/main/java/com/orange/portfolio/repositories/UserRepository.java
+++ src/main/java/com/orange/portfolio/repositories/UserRepository.java
@@ -6,5 +6,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+
     void delete(User user);
+
+    User findByEmail(String email);
 }
