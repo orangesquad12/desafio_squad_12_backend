e241be105fcdef7309b62655c3dc4f13a49490b7
Debora <silvadeborafisio@gmail.com>
2024-02-01 15:21:01 -0300

    fix: refactoring configuration class to autheticate user

--- src/main/java/com/orange/portfolio/configuration/SecurityConfiguration.java
+++ src/main/java/com/orange/portfolio/configuration/SecurityConfiguration.java
@@ -1,10 +1,12 @@
 package com.orange.portfolio.configuration;
 
+import com.orange.portfolio.services.auth.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,10 +18,16 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration {
+public class SecurityConfiguration{
 
     @Autowired
     private SecurityFilter securityFilter;
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -27,23 +35,26 @@ public class SecurityConfiguration {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize ->  authorize
-                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/project").authenticated()
-                        .requestMatchers(HttpMethod.DELETE, "/project/{userId}").authenticated()
-                        .requestMatchers(HttpMethod.PUT, "/project/{userId}").authenticated()
-                        .requestMatchers(HttpMethod.DELETE, "/users/{id}").authenticated()
-                        .requestMatchers(HttpMethod.PUT, "/users/{id}").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/project").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/api/project/{userId}").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/api/project/{userId}").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/api/users/image/{id}").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/api/users/{id}").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/api/users/{id}").authenticated()
                         .anyRequest().permitAll()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
+
     @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity) throws Exception {
+        AuthenticationManagerBuilder builder = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+        builder.userDetailsService(authenticationService);
+        return builder.build();
     }
 
 
