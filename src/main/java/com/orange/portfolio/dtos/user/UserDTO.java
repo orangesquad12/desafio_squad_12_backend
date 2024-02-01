f0a2578375e3afc7b24df59229c93d4092020b20
Debora <silvadeborafisio@gmail.com>
2024-01-31 21:49:29 -0300

    refactor: chance UserDTO from record to class and inserting new constructor

--- src/main/java/com/orange/portfolio/dtos/user/UserDTO.java
+++ src/main/java/com/orange/portfolio/dtos/user/UserDTO.java
@@ -1,4 +1,97 @@
 package com.orange.portfolio.dtos.user;
 
-public record UserDTO(String firstName, String lastName, String image, String country, String email, String password) {
+public class UserDTO {
+
+    private String firstName;
+    private String lastName;
+    private String image;
+    private String country;
+    private String email;
+    private String password;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public UserDTO(String firstName, String lastName, String image, String country, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.image = image;
+        this.country = country;
+        this.email = email;
+        this.password = password;
+    }
+
+    public UserDTO(String firstName, String lastName,
+                   String country, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", image='" + image + '\'' +
+                ", country='" + country + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
