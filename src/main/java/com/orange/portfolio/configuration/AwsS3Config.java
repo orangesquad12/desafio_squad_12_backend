f46c45b02ed786619e3b7158fe57fa4f870e521e
Debora <silvadeborafisio@gmail.com>
2024-02-01 15:33:30 -0300

    feat: inserting settings for integration with AWS S3

---
+++ src/main/java/com/orange/portfolio/configuration/AwsS3Config.java
@@ -0,0 +1,38 @@
+package com.orange.portfolio.configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsS3Config {
+
+    @Value("${aws.s3.bucketName}")
+    private String bucketName;
+
+    @Value("${aws.s3.endpointUrl}")
+    private String endpointUrl;
+
+    @Value("${aws.s3.region}")
+    private String region;
+
+    @Value("${aws.s3.accessKeyId}")
+    private String accessKey;
+
+    @Value("${aws.s3.secretKey}")
+    private String secretKey;
+
+    @Bean
+    public AmazonS3Client amazonS3Client(){
+        BasicAWSCredentials credentials = new BasicAWSCredentials(
+                accessKey, secretKey);
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
+    }
+}
