4c94b2f4912f7d67aef9f98f319dcfac638b35b4
Debora <silvadeborafisio@gmail.com>
2024-02-02 16:43:33 -0300

    refactor: inserting script project_tags

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -9,6 +9,11 @@ CREATE TABLE tb_user (
    CONSTRAINT pk_tb_user PRIMARY KEY (id)
 );
 
+CREATE TABLE project_tags (
+  project_id BIGINT NOT NULL,
+   tags VARCHAR(255) NULL
+);
+
 CREATE TABLE tb_project (
   id BIGINT AUTO_INCREMENT NOT NULL,
    client_id BIGINT NULL,
@@ -20,3 +25,5 @@ CREATE TABLE tb_project (
 );
 
 ALTER TABLE tb_project ADD CONSTRAINT FK_TB_PROJECT_ON_CLIENT FOREIGN KEY (client_id) REFERENCES tb_user (id);
+
+ALTER TABLE project_tags ADD CONSTRAINT fk_project_tags_on_project FOREIGN KEY (project_id) REFERENCES tb_project (id);
