0ce866cbafc76e6a1333f51ff375d4a1a6413348
Debora <silvadeborafisio@gmail.com>
2024-02-02 13:38:25 -0300

    refactor: inserting validation for login

--- src/main/java/com/orange/portfolio/dtos/user/LoginDTO.java
+++ src/main/java/com/orange/portfolio/dtos/user/LoginDTO.java
@@ -2,5 +2,5 @@ package com.orange.portfolio.dtos.user;
 
 import jakarta.validation.constraints.NotEmpty;
 
-public record LoginDTO(@NotEmpty(message = "Mandatory field") String email, String password) {
+public record LoginDTO(@NotEmpty(message = "Mandatory field") String email, @NotEmpty(message = "Mandatory field") String password) {
 }
