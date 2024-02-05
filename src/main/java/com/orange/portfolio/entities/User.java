3b83b76eab5cdaab6dda1cd54a2fe1a3a1d7ad40
Debora <silvadeborafisio@gmail.com>
2024-02-04 22:02:36 -0300

    refactor: refactoring validation in the lastName field

--- src/main/java/com/orange/portfolio/entities/User.java
+++ src/main/java/com/orange/portfolio/entities/User.java
@@ -18,7 +18,7 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String lastName;
     @Column(nullable = false, unique = true)
     private String firstName;
