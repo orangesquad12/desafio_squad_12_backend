8fedd3d02d56a5bcc5b31e19ba89bfcbd7710765
Debora <silvadeborafisio@gmail.com>
2024-02-04 21:58:32 -0300

    refactor: changing S3 configuration class name

--- src/main/java/com/orange/portfolio/configuration/AwsS3Configuration.java
+++ src/main/java/com/orange/portfolio/configuration/AwsS3Configuration.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AwsS3Config {
+public class AwsS3Configuration {
 
     @Value("${aws.s3.bucketName}")
     private String bucketName;
